f7f54e2eeb69ddc99956c98a47c3ef31a23c6d77
janda <158464459@qq.com>
2017-11-28 17:24:15 +0800

    设置邮箱匿名发送

--- smtp_email.py
+++ smtp_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from email.mime.text import MIMEText
+from email.header import Header
 import email.mime.multipart
 import email.mime.text
 import smtplib
@@ -12,20 +13,21 @@ __author__ = "作者"
 
 def send_email(msgTo, title, text):
     msg = email.mime.multipart.MIMEMultipart()
-    msgFrom = 'xx@126.com'  # 从该邮箱发送
-    smtpSever = 'smtp.126.com'  # 126邮箱的smtp Sever地址
-    smtpPort = '25'  # 网易邮箱、默认25
-    sqm = 'xx'  # 在登录smtp时需要login中的密码应当使用授权码而非账户密码
-    msg['from'] = msgFrom
-    msg['to'] = msgTo
-    msg['subject'] = title
-    txt = email.mime.text.MIMEText(text)
-    msg.attach(txt)
-    smtp = smtplib.SMTP()
+    msgFrom = "xx@126.com"  # 从该邮箱发送
+    smtpSever = "smtp.126.com"  # 126邮箱的smtp Sever地址
+    smtpPort = "25"  # 网易邮箱、默认25
+    sqm = "xx"  # 在登录smtp时需要login中的密码应当使用授权码而非账户密码
+    # msg["from"] = msgFrom
+    msg["from"] = Header("机器人助理", "utf-8")
+    msg["to"] = msgTo
+    msg["subject"] = title
     '''
         smtplib的connect（连接到邮件服务器）、login（登陆验证）、sendmail（发送邮件）
     '''
     try:
+        txt = email.mime.text.MIMEText(text)
+        msg.attach(txt)
+        smtp = smtplib.SMTP()
         smtp.connect(smtpSever, smtpPort)
         smtp.login(msgFrom, sqm)
         smtp.sendmail(msgFrom, msgTo, str(msg))
